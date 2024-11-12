007d2529e01235abb739dc18606add5d9c4fecb4
deyong <xzywork@163.com>
2024-11-12 22:51:28 +0800

    修改layout的bug

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,16 +1,14 @@
-export default function Layout({ children }) {
+import React from 'react'
+
+interface LayoutProps {
+  children: React.ReactNode
+}
+
+export default function Layout({ children }: LayoutProps) {
   return (
     <div className="min-h-screen tech-grid-bg">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        {/* 导航栏示例 */}
-        <nav className="glass-effect fixed top-0 left-0 right-0 z-50">
-          {/* 导航内容 */}
-        </nav>
-        
-        {/* 主要内容区域 */}
-        <main>
-          {children}
-        </main>
+        {children}
       </div>
     </div>
   )
